15d0f0c21ac3a9ce9640902ed229eb7b13e5d753
tzivi618 <tzivi618@gmail.com>
2025-05-26 14:10:18 +0300

    add lessons

---
+++ src/app/components/registrants/registrants.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-registrants',
+  standalone: true,
+  imports: [],
+  templateUrl: './registrants.component.html',
+  styleUrl: './registrants.component.css'
+})
+export class RegistrantsComponent {
+
+}
